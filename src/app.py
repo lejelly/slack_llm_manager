eed29e36b69994df25b8f10945ab49ceb5c1628e
lejelly <j.seongcheol118@gmail.com>
2024-01-21 10:25:32 +0000

    slackbot

--- src/app.py
+++ src/app.py
@@ -3,11 +3,17 @@ import logging
 from slack_bolt import App, Ack, Say, BoltContext, Respond
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from slack_sdk import WebClient
+from  openai import OpenAI
 
 # デバッグレベルのログを有効化
 logging.basicConfig(level=logging.DEBUG)
 # これから、この app に処理を設定していきます
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+gpt = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
 
 @app.command("/modal-command")
 def handle_some_command(ack: Ack, body: dict, client: WebClient):
@@ -30,7 +36,7 @@ def handle_some_command(ack: Ack, body: dict, client: WebClient):
                         "type": "plain_text_input",
                         "action_id": "input-element",
                     },
-                    "label": {"type": "plain_text", "text": "何らかのデータ登録"},
+                    "label": {"type": "plain_text", "text": "どんな人をお探しですか？"},
                 },
             ],
             "notify_on_close": True,
@@ -49,6 +55,8 @@ def handle_view_closed(ack: Ack, view: dict, logger: logging.Logger):
 @app.view("modal-id")
 def handle_view_events(ack: Ack, view: dict, client: WebClient):
     # まず「処理中...」である旨を伝えます
+    inputs = view["state"]["values"]
+    question = inputs.get("question-block", {}).get("input-element", {}).get("value")
     ack(
         response_action="update",
         view={
@@ -61,16 +69,15 @@ def handle_view_events(ack: Ack, view: dict, client: WebClient):
                     "type": "section",
                     "text": {
                         "type": "plain_text",
-                        "text": "処理中です... このモーダルを閉じずにしばらくお待ちください :bow:",
+                        "text": "少々お待ちください、ぴったりの人がいますよ！",
                     },
                 }
             ],
         },
     )
 
-    # 何か時間がかかる処理をシミュレートしているだけです
-    import time
-    time.sleep(3.5)  # 3.5 秒かかります
+    # LLM による応答を取得します
+    responce = query_gpt_chat(question)
 
     # 結果を待った後 views.update API を非同期で呼び出して再度更新をかけます
     client.views_update(
@@ -78,12 +85,12 @@ def handle_view_events(ack: Ack, view: dict, client: WebClient):
         view={
             "type": "modal",
             "callback_id": "modal-id",
-            "title": {"type": "plain_text", "text": "テストモーダル"},
+            "title": {"type": "plain_text", "text": "コミュニティマネージャー"},
             "close": {"type": "plain_text", "text": "閉じる"},
             "blocks": [
                 {
                     "type": "section",
-                    "text": {"type": "plain_text", "text": "正常に完了しました！"},
+                    "text": {"type": "plain_text", "text": "この人と会ってみてはどうでしょう？\n"+responce},
                 }
             ],
         },
@@ -91,6 +98,38 @@ def handle_view_events(ack: Ack, view: dict, client: WebClient):
         # hash=view.get("hash"),
     )
 
+def query_gpt_chat(input_str):
+    try:
+        f = open('/Dev/slack_bot/prompt/system.txt', 'r', encoding='UTF-8')
+        system_content = f.read()
+        f.close()
+        f = open('/Dev/slack_bot/prompt/fewshot.txt', 'r', encoding='UTF-8')
+        fewshot = f.read()
+        f.close()
+        f = open('/Dev/slack_bot/prompt/user.txt', 'r', encoding='UTF-8')
+        user = f.read()
+        f.close()
+        f = open('/Dev/slack_bot/prompt/community_info.txt', 'r', encoding='UTF-8')
+        community_info = f.read()
+        f.close()
+        
+        user_content = user + fewshot + "[ユーザからの要望]"+"\n\n"+input_str+"\n\n"+community_info+"\n\n\n"+"[あなたの回答]"
+        
+        response = gpt.chat.completions.create(
+            model="gpt-4-1106-preview",
+            temperature = 0,
+            messages=[
+                {"role": "system", "content": system_content},
+                {"role": "user", "content": user_content}
+            ],
+        )
+        result = response.choices[0].message.content
+        return result
+    except AttributeError as e:
+        error_message = f"Error: {e}"
+        print(error_message)
+        result = error_message 
+        return result
 
 if __name__ == "__main__":
     # ソケットモードのコネクションを確立
